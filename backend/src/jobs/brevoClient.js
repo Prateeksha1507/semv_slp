36cf805fac76b5198c30c7a4ce4ee388e313bffe
VijaySamant4368 <vijaysamant4368@gmail.com>
2025-12-02 00:05:30 +0530

    Use API key for HTTPS request

--- backend/src/jobs/brevoClient.js
+++ backend/src/jobs/brevoClient.js
@@ -1,19 +1,16 @@
 import axios from "axios";
 
-const BREVO_API_KEY = process.env.BREVO_PASS;
-const EMAIL_FROM = process.env.EMAIL_FROM;
+const BREVO_API_KEY = process.env.BREVO_API_KEY;
+const EMAIL_FROM_EMAIL = process.env.EMAIL_FROM_EMAIL;
+const EMAIL_FROM_NAME = process.env.EMAIL_FROM_NAME;
 
 if (!BREVO_API_KEY) {
-  console.warn("⚠ BREVO_PASS (Brevo API key) is not set");
-}
-
-if (!EMAIL_FROM) {
-  console.warn("⚠ EMAIL_FROM is not set");
+  console.warn("⚠ BREVO_API_KEY (Brevo API key) is not set");
 }
 
 export async function sendEmail({ to, subject, text, html }) {
   const payload = {
-    sender: { email: EMAIL_FROM },
+    "sender":{"email":EMAIL_FROM_EMAIL,"name":EMAIL_FROM_NAME},
     to: [{ email: to }],
     subject,
     textContent: text,
